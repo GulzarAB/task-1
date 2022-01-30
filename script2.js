121bc0c5454c4a21a752d1c5895ec9c2bae65504
Gulzar Abdullabaeva <gulzar.abdullabaeva.kk@gmail.com>
2022-01-30 16:25:24 +0600

    fix:second commit

---
+++ script2.js
@@ -0,0 +1,5 @@
+// 2. Простые сообщения: сохраните сообщение в переменной и выведите это сообщение. Затем замените значение переменной другим сообщением и выведите новое сообщение
+let firstText = "Gulzar";
+document.write(firstText);
+firstText = "Abdullabaeva";
+document.write(firstText);
---
+++ script3.js
@@ -0,0 +1,4 @@
+// 3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+let userName = "Gulzar";
+let userEmail = "gulzar.abdullabaeva.kk@gmail.com";
+document.write("Имя:"+ userName + "Почтовый адрес:" + userEmail);
